7581ff56509df3bf62d419714be876bfff9e3dd1
Sahan Perera <sahanvimukthi00@gmail.com>
2024-06-11 03:02:29 +0530

    chore: index.js updated

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import express from 'express';
+import 'dotenv/config';
+import authMiddleware from './middleware/authMiddleware';
+import authRouter from './api/auth/auth.router';
+import productRouter from './api/product/product.router';
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
+app.use('/api/auth', authRouter);
+app.use(authMiddleware);
+
+app.use('/api/product', productRouter);
 
 app.listen(PORT, () => console.log('Server Started'));
